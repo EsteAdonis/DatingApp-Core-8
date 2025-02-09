fb50fb9132d06720a926e3f43eda2fbd54ed15cb
EsteAdonis <julio.e.ochoa.p@gmail.com>
2025-02-08 22:35:20 -0600

    Initializing API Project

---
+++ API/Program.cs
@@ -0,0 +1,34 @@
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// builder.Services.AddEndpointsApiExplorer();
+// builder.Services.AddSwaggerGen();
+// DJ: The framework is responsible to create a new instance the DataContext Class and thes disposing of that class that we inject after we finished using it.
+builder.Services.AddControllers();
+builder.Services.AddDbContext<DataContext>( opt => 
+{
+  opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+// if (app.Environment.IsDevelopment())
+// {
+//     app.UseSwagger();
+//     app.UseSwaggerUI();
+// }
+
+// app.UseHttpsRedirection();
+
+// app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
