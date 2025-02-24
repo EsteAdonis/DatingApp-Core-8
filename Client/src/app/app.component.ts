fac8d013eb06558de5f61f13ae40bcf61681f229
EsteAdonis <julio.e.ochoa.p@gmail.com>
2025-02-23 19:24:00 -0600

    Adding a home page

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -4,11 +4,14 @@ import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavComponent],
+  imports: [RouterOutlet, 
+            NavComponent, 
+            HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
