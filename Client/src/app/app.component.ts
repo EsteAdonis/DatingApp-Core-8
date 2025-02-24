a8381691f2ede5f383d01ddbb06731b2410de3ee
EsteAdonis <julio.e.ochoa.p@gmail.com>
2025-02-23 20:48:08 -0600

    Parent to child communication (Signal)

--- Client/src/app/app.component.ts
+++ Client/src/app/app.component.ts
@@ -1,8 +1,6 @@
-import { HttpClient } from '@angular/common/http';
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
-import { User } from './_models/user';
 import { AccountService } from './_services/account.service';
 import { HomeComponent } from "./home/home.component";
 
@@ -16,13 +14,9 @@ import { HomeComponent } from "./home/home.component";
   styleUrl: './app.component.css'
 })
 export class AppComponent implements OnInit {
-  http = inject(HttpClient);
   private accountService = inject(AccountService);
-  title = 'DatingApp';
-  users: any;
 
   ngOnInit(): void {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -33,13 +27,4 @@ export class AppComponent implements OnInit {
     const user = JSON.parse(userString);
     this.accountService.currentUser.set(user);
   }
-
-  getUsers() {
-    this.http.get<any>('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: error => console.log(error),
-      complete: ()=> console.log('Request has completed')
-    })
-  }
-
 }
