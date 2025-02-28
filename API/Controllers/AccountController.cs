a105b25ac88d7450129dc78606610fe8fcb53902
EsteAdonis <julio.e.ochoa.p@gmail.com>
2025-02-27 20:54:54 -0600

    Entending AppUser & adding a DateOnly extension to calculate age

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -14,21 +14,23 @@ public class AccountController(DataContext context, ITokenService tokenService):
   public async Task<ActionResult<UserDto>> Register(RegisterDto registerDto)
   {
     if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
+    
+    return Ok();
 
-    using var hmac = new HMACSHA512();
-    var user = new AppUser
-    {
-      UserName = registerDto.Username,
-      PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
-      PasswordSalt = hmac.Key
-    };
-    context.Users.Add(user);
-    await context.SaveChangesAsync();
+    // using var hmac = new HMACSHA512();
+    // var user = new AppUser
+    // {
+    //   UserName = registerDto.Username,
+    //   PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
+    //   PasswordSalt = hmac.Key
+    // };
+    // context.Users.Add(user);
+    // await context.SaveChangesAsync();
 
-    return new UserDto {
-      Username = user.UserName,
-      Token = tokenService.CreateToken(user)
-    };
+    // return new UserDto {
+    //   Username = user.UserName,
+    //   Token = tokenService.CreateToken(user)
+    // };
   }
 
   [HttpPost("login")]
