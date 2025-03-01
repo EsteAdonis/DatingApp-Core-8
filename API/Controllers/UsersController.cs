0c53fbf683f04d4e4e32d3e4795f86d0a165b695
EsteAdonis <julio.e.ochoa.p@gmail.com>
2025-02-28 19:02:02 -0600

    Adding & configure AutoMapper

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -4,25 +4,28 @@ using API.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authorization;
 using API.Interfaces;
+using API.DTOs;
+using AutoMapper;
 namespace API.Controllers;
 
 [Authorize]
-public class UsersController(IUserRepository userRepository) : BaseApiController
+public class UsersController(IUserRepository userRepository, IMapper mapper) : BaseApiController
 {
   [HttpGet]
-	public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+	public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
 	{
 		var users = await userRepository.GetUserAsync();
-		return Ok(users);
+		var usersToReturn = mapper.Map<IEnumerable<MemberDto>>(users);
+		return Ok(usersToReturn);
 	}
 	
 	[HttpGet("{username}")]
-	public async Task<ActionResult<AppUser>> GetUser(string username)
+	public async Task<ActionResult<MemberDto>> GetUser(string username)
 	{
 		var user = await userRepository.GetUserByUsernameAsyunc(username);
 		if (user == null) return NotFound();
 		
-		return user;
+		return mapper.Map<MemberDto>(user);
 	}
 }
 
